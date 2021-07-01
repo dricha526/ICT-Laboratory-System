627361cfed9826f04586fb3036f9f3fc2f31a3e2
dricha526 <75879835+dricha526@users.noreply.github.com>
2021-07-01 17:52:19 +0530

    Add files via upload

---
+++ member/login.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<title></title>
+
+	<!-- bootstrap -->
+	<link rel="stylesheet" type="text/css" href="../assets/custom/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../assets/custom/css/bootstrap-table.css">
+	<link rel="stylesheet" type="text/css" href="../assets/custom/css/datepicker.css">
+	<link rel="stylesheet" type="text/css" href="../assets/custom/css/datepicker3.css">
+	<link rel="stylesheet" type="text/css" href="../assets/custom/css/styles.css">
+	
+
+
+	<!-- datatables -->
+	<link rel="stylesheet" type="text/css" href="../assets/datatables/css/jquery.dataTables.min.css">
+
+	<!-- fontawesome -->
+	<link rel="stylesheet" type="text/css" href="../assets/fontawesome/css/font-awesome.min.css">
+
+
+	<!-- toastr -->
+	<link rel="stylesheet" type="text/css" href="../assets/toastr/css/toastr.css">
+	<!-- custom -->
+	<link rel="stylesheet" type="text/css" href="../assets/mycustom/css/styles.css">
+	
+	
+	
+
+
+
+</head>
+<body >
+<div class="container-fluid">
+	<div class="container">
+
+		<div class="row">
+			<div class="col-md-12">
+				<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+					<div class="login-panel panel panel-default">
+						<div class="panel-heading"> ICT Laboratory System</div>
+						<div class="panel-body">
+					
+						
+			
+				<form class="frm_memberlogin">
+				<fieldset>
+					<h4 class="alert bg-primary">Borrower Login</h4>
+					<div class="form-group">
+						<label>ID Number</label>
+						<input type="number" name="id_number" class="form-control" autofocus="on">
+					</div>
+					<div class="form-group">
+						<button class="btn btn-primary btn-block">Log in</button>
+						<br>
+						<a href="../"> Go to Admin Panel</a>
+						
+					</div>
+					</fieldset>
+				</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	
+	</div>
+
+
+	
+
+</body>
+</html>
+
+<?php include 'footer.php'; ?>
